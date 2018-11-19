f9c0bf4d88d8a1d5300299ad6846bb4e88e67c01
shubhamdevkate1998 <35358234+shubhamdevkate1998@users.noreply.github.com>
2018-11-19 12:43:03 +0530

    added macros.py

---
+++ macros.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+###########################################################################
+#    Lios - Linux-Intelligent-Ocr-Solution
+#    Copyright (C) 2011-2015 Nalin.x.Linux GPL-3
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###########################################################################
+
+import os
+
+home_dir = os.environ['HOME']
+
+tmp_dir = "/tmp/Lios/"
+
+supported_image_formats = ["png","pnm","jpg","jpeg","tif","tiff","bmp","pbm","ppm"]
+
+supported_text_formats = ["txt","text"]
+
+supported_pdf_formats = ["pdf"]
+
+version = "2.0"
+
+logo_file = "/usr/share/lios/lios.png"
+
+
+readme_file = "/usr/share/lios/readme.text"
+
+app_name = "Linux-intelligent-ocr-solution"
+
+source_link = "https://gitlab.com/Nalin-x-Linux/lios-3"
+
+home_page_link = "http://sourceforge.net/projects/lios/"
+
+video_tutorials_link = "https://www.youtube.com/playlist?list=PLn29o8rxtRe1zS1r2-yGm1DNMOZCgdU0i"
